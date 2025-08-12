ad5087099fa0fd4af70e94d8e2e558000c0eaaa4
Aline Almeida <a.andrade.de.almeida@avanade.com>
2025-08-12 18:11:20 -0300

    definindo a navegação da página

--- src/pages/certificado/certificado.ts
+++ src/pages/certificado/certificado.ts
@@ -1,9 +1,10 @@
+import { RouterLink } from '@angular/router';
 import { SecondaryButton } from './../../app/_components/secondary-button/secondary-button';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-certificado',
-  imports: [SecondaryButton],
+  imports: [SecondaryButton, RouterLink],
   templateUrl: './certificado.html',
   styleUrl: './certificado.css'
 })
